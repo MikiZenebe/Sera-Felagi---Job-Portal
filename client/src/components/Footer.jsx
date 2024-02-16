a334f025409c82461491008b6d845cfd0251495b
MikiZenebe <mickyaszenebe@gmail.com>
2024-02-15 20:24:57 -0600

    Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { Github, Instagram, Twitter } from "lucide-react";
 
 export default function Footer() {
   return (
     <>
-      <Box className="w-full flex justify-between items-center py-3 backdrop-blur-lg">
-        Miki
-      </Box>
+      <Flex
+        bg={useColorModeValue("#633da5ea", "#183242")}
+        color={useColorModeValue("gray.500", "white")}
+        className="w-full h-[120px] flex justify-between items-center py-3 "
+      >
+        <Box className="flex flex-col justify-center items-center mx-auto text-gray-300 gap-2">
+          <Text>Developed By Mikiyas Zenebe</Text>
+          <Text>© All Rights Reserved 2024</Text>
+          <Box className="flex items-center gap-3">
+            <Instagram className="bg-orange-300 text-black p-1 rounded-full" />
+            <Github className="bg-orange-300 text-black p-1 rounded-full" />
+            <Twitter className="bg-orange-300 text-black p-1 rounded-full" />
+          </Box>
+        </Box>
+      </Flex>
     </>
   );
 }
