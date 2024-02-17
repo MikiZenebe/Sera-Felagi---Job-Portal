7d8a0fbd3c8b153b6db3c93874627b117112f950
MikiZenebe <mickyaszenebe@gmail.com>
2024-02-17 15:59:50 -0600

    login

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,5 +1,5 @@
 import User from "../models/userModel.js";
-import { hashString } from "../utils/index.js";
+import { compareString, createJWT, hashString } from "../utils/index.js";
 
 export const register = async (req, res, next) => {
   const { name, username, email, password, userType } = req.body;
@@ -23,6 +23,48 @@ export const register = async (req, res, next) => {
     });
 
     res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    //Validation
+    if (!email || !password) {
+      next("Please Provide User Credentials");
+    }
+
+    //Find a user by email
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      next("Invalid email or password");
+      return;
+    }
+
+    //Compare Password
+    const isMatch = await compareString(password, user?.password);
+
+    if (!isMatch) {
+      next("Invalid email or password");
+      return;
+    }
+
+    //remove the password from the user object for security reasons.
+    user.password = undefined;
+
+    const token = createJWT(user?._id);
+
+    res.status(200).json({
+      success: true,
+      message: "Login successfully",
+      user,
+      token,
+    });
   } catch (error) {
     console.log(error);
     res.status(404).json({ message: error.message });
